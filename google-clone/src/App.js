479df3024a378229be06aa9767114cc336fe88ac
fasih4444 <arainfaisal826@gmail.com>
2020-08-21 16:17:10 +0500

    init

---
+++ google-clone/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hey Fasih Fam!
+      Let's Build the Clone. 🚀</h1>
+
+      {/* Home {the one with search on} */}
+      <Home />
+
+
+
+      {/*SearchPage {The result page} */}
+
+
+
+    </div>
+  );
+}
+
+export default App;
